b48145adfede791fb67e4bfebe0ca57ec470fa44
ProyectoCode <hxyl3y1995@gmail.com>
2022-02-12 17:16:25 -0500

    Creo la clase bar para dibujar las barras del juego Ping Pong

--- main.js
+++ main.js
@@ -28,13 +28,63 @@
         this.board = board;
         this.ctx = canvas.getContext("2d");        
     } 
+
+    
+    self.BoardView.prototype = {
+    draw : function (){
+        for (var i = this.board.elements.length - 1; i >= 0; i--) {
+            var el = this.board.elements[i];
+            draw(this.ctx, el);                
+          };
+       }
+    }
+    /* Funcion para recibir los parametros de las barras en x,y,ancho y alto y el dibujo de las barras*/
+    function draw (ctx,element){  
+        if(element !== null && element.hasOwnProperty("kind")){
+           switch(element.kind){
+             case "rectangle":
+              ctx.fillRect(element.x,element.y,element.width,element.height);
+              break;
+             }  
+        }     
+       
+        }
+})();
+
+(function () {
+    /* creamos la clase bar para las barras rectangulares del juego*/
+    self.Bar = function (x,y,width,height,board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";  
+             
+    }
+    /* Función para moviemiento de las barras*/
+    self.Bar.prototype = {
+        down: function () {   
+                   
+        },
+        up: function () {  
+                   
+        }
+       
+    }
 })();
 
 window.addEventListener("load", main);
 /*intanciamos nuevos objetos de las clases*/
 function main(){
     var board = new Board(800,400);
+    var bar = new Bar(20,100,40,100, board);
+    var bar2 = new Bar(735,100,40,100, board);
     var canvas = document.getElementById('canvas');
     var board_view = new BoardView(canvas,board);
+
+
+    board_view.draw();
 
 }

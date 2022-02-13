2b37ef161e04f93628a116bb6f96b497824e276d
ProyectoCode <hxyl3y1995@gmail.com>
2022-02-13 15:41:13 -0500

    Creamos la clase pelota y le asignamos parametros para darle movimientos

--- main.js
+++ main.js
@@ -13,7 +13,7 @@
        self.Board.prototype ={
        get elements(){
            var elements = this.bars;
-           //elements.push(this.ball);
+           elements.push(this.ball);
            return elements;
 
            }
@@ -34,11 +34,16 @@
         clean: function(){
             this.ctx.clearRect(0,0,this.board.width,this.board.height);
         },
-    draw : function (){
+        draw : function (){
         for (var i = this.board.elements.length - 1; i >= 0; i--) {
             var el = this.board.elements[i];
             draw(this.ctx, el);                
           };
+       },
+       play: function(){
+        this.clean();
+        this.draw();
+
        }
     }
     /* Funcion para recibir los parametros de las barras en x,y,ancho y alto y el dibujo de las barras*/
@@ -48,10 +53,30 @@
              case "rectangle":
               ctx.fillRect(element.x,element.y,element.width,element.height);
               break;
+              case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x,element.y,element.radius,0,7);
+                ctx.fill();
+                ctx.closePath();
+                break;
              }  
        
        
         }
+})();
+//Creamos la funcion de la pelota de PingPong
+(function(){
+    self.ball= function (x,y,radius,board) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board; 
+        board.ball = this;
+        this.kind = "circle"  
+    }
+
 })();
 
 (function () {
@@ -64,7 +89,7 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";  
-        this.speed = 10;
+        this.speed = 6;
              
     }
     /* Función para moviemiento de las barras*/
@@ -83,18 +108,19 @@
        
     }
 })();
-
+// Instanciamos los objetos 
 var board = new Board(800,400);
 var bar = new Bar(20,100,40,100, board);
 var bar_2 = new Bar(735,100,40,100, board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas,board);
+var ball = new ball (350, 100, 10, board );
 
 /*creamos la clases*/
 
 
 
-/* crear evento con el ketdown para que el jugador presiona teclas para jugar al PING PONG*/
+/* crear evento con el keydown para que el jugador presiona teclas para jugar al PING PONG*/
 document.addEventListener("keydown",function (ev){
     ev.preventDefault();
     if(ev.keyCode == 38){
@@ -104,17 +130,18 @@ document.addEventListener("keydown",function (ev){
          bar.down();
      }else if(ev.keyCode === 87){
         ev.preventDefault();
+        //w
          bar_2.up();
      }else if(ev.keyCode === 83){
+         //s
         bar_2.down(); 
      }
 });
 /*animación de la barras*/
 window.requestAnimationFrame(controller);
-/*intanciamos nuevos objetos de las clases*/
+/*Creamos un nuevo método que se llame PLAY*/
 function controller(){
-board_view.clean();
-board_view.draw();
+board_view.play();
 window.requestAnimationFrame(controller);
 
 }
